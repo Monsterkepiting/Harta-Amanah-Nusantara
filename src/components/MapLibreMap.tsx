f83a7b4a6b0a41c4f11f8b9f49213dcf179ab36f
Monsterkepiting <sundaymorningkopi@gmail.com>
2025-11-30 11:05:23 +0800

    Update MapLibreMap.tsx

--- src/components/MapLibreMap.tsx
+++ src/components/MapLibreMap.tsx
@@ -1,1 +1,18 @@
+import { useEffect } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+export default function MapLibreMap() {
+  useEffect(() => {
+    const map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://demotiles.maplibre.org/style.json',
+      center: [106.8272, -6.1751],
+      zoom: 12
+    });
+    return () => map.remove();
+  }, []);
+
+  return <div id="map" style={{ width: '100%', height: '500px' }} />;
+}
 
