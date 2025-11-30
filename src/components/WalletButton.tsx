34acb253c00851b0ae1ccc265b7f408a61fc8f67
Monsterkepiting <sundaymorningkopi@gmail.com>
2025-11-30 11:05:03 +0800

    Update WalletButton.tsx

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,1 +1,13 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
+export default function WalletButton() {
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+
+  return (
+    <button onClick={connectWithMetamask}>
+      {address ? `${address.slice(0,6)}...${address.slice(-4)}` : 'Connect Wallet'}
+    </button>
+  );
+}
 
