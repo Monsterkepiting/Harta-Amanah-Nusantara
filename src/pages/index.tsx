fccb08c71869a1fb0dfa37e35706cdedc55e2798
Monsterkepiting <sundaymorningkopi@gmail.com>
2025-11-30 11:06:35 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,12 @@
+import WalletButton from '../components/WalletButton';
+import MapLibreMap from '../components/MapLibreMap';
+
+export default function Home() {
+  return (
+    <div>
+      <WalletButton />
+      <MapLibreMap />
+    </div>
+  );
+}
 
