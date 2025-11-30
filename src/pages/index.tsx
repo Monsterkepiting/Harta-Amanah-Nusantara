463416f86a94d79ab7db7ebe7043ec14c4061f2a
Monsterkepiting <sundaymorningkopi@gmail.com>
2025-11-30 12:54:14 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import WalletButton from '../components/WalletButton';
-import MapLibreMap from '../components/MapLibreMap';
+import WalletButton from "../components/WalletButton";
+import MapLibreMap from "../components/MapLibreMap";
 
 export default function Home() {
   return (
     <div>
-      <WalletButton />
-      <MapLibreMap />
+      <header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "12px 24px", backgroundColor: "#1a1a1a", color: "white" }}>
+        <h1>Harta Amanah Nusantara</h1>
+        <WalletButton />
+      </header>
+      <main style={{ padding: "24px" }}>
+        <MapLibreMap />
+      </main>
     </div>
   );
 }
-
